d4392a109ec7f2c4a9bc0f617acc63f7d6e7b20f
leechungrae <leechungrae@gmail.com>
2017-05-02 23:15:04 +0900

    add music and Page logic

--- PL_TeamProject.py
+++ PL_TeamProject.py
@@ -2,6 +2,10 @@ import pygame
 
 pygame.init()
 
+#오디오 출력용
+pygame.mixer.music.load('sound/ponyo.wav')
+pygame.mixer.music.play(0)
+
 widthSize = 500
 heightSize = 400
 
@@ -15,21 +19,31 @@ y = heightSize/2
 
 
 finish = False
+Page = 1
+
 while not finish:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finish = True
 
+    pressd = pygame.key.get_pressed()  # 키 이벤트
 
-    #밑에 게임로직 설계
-    game_screen.fill((0, 200, 0))  #배경색 갱신해줘야한다
+    if Page == 1:
+        game_screen.fill((200, 200, 0))  # 배경색 갱신해줘야한다
 
-    pressd = pygame.key.get_pressed()  #키 이벤트
+        if pressd[pygame.K_UP]:   Page = 2
 
+    elif Page == 2:
+        #밑에 게임로직 설계
+        game_screen.fill((0, 200, 0))  #배경색 갱신해줘야한다
 
-    if pressd[pygame.K_RIGHT]:  x += 5
-    if pressd[pygame.K_LEFT]:   x -= 5
+        if pressd[pygame.K_RIGHT]:  x += 5
+        if pressd[pygame.K_LEFT]:   x -= 5
 
-    pygame.draw.rect(game_screen, (255,20,20), pygame.Rect(x, y, 10, 10))  # 방향키 처리
+        pygame.draw.rect(game_screen, (255,20,20), pygame.Rect(x, y, 10, 10))  # 방향키 처리
+        if pressd[pygame.K_DOWN]:   Page = 3
+
+    elif Page == 3:
+        game_screen.fill((200, 200, 200))  # 배경색 갱신해줘야한다
 
     pygame.display.flip() #프레임 갱신
