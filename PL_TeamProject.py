a14165d3d7ee9d8ffaa6dc1f4ac76dacb36c45bf
leechungrae <leechungrae@gmail.com>
2017-05-04 18:58:00 +0900

    Clean up Code

--- PL_TeamProject.py
+++ PL_TeamProject.py
@@ -3,20 +3,24 @@ import pygame
 
 pygame.init()
 
+#----------------------------------## 게임 기본 설정 관련 ##---------------------------------------
+
+
+#게임 전체 스크린 사이즈
+widthSize = 500
+heightSize = 400
+
+
+#게임 스크린 사이즈와 게임 제목 설정
+game_screen = pygame.display.set_mode((widthSize,heightSize))
+pygame.display.set_caption("test")
+
 #오디오 출력용
 pygame.mixer.music.load('sound/ponyo.wav')
 pygame.mixer.music.play(0)
 
-widthSize = 500
-heightSize = 400
+#-----------------------------------## 게임 관련 함수 ##---------------------------------------
 
-game_screen = pygame.display.set_mode((widthSize,heightSize))  #스크린 사이즈 지정
-pygame.display.set_caption("test") #제목 지정
-
-
-#키보드 이동용 전역변수 초기위치지정용도
-x = widthSize/2
-y = heightSize/2
 
 def show_img(ourScreen, img_name, x_Position,y_Position) :
     myimg = pygame.image.load(img_name)
@@ -28,23 +32,29 @@ def show_text(ourScreen,text,x_Position, y_Position):
     textSurface = text_Font.render(text, True, (0, 100, 0))
     ourScreen.blit(textSurface, (x_Position, y_Position))
 
-#------------------------------------
+
+#-----------------------------------## 게임 사용 변수 ##---------------------------------------
+
+#키보드 이동용 전역변수 초기위치지정용도
+x = widthSize/2
+y = heightSize/2
+
 
 finish = False
 Page = 1
 
 
-
+#-----------------------------------## 게임 로직 시작 ##---------------------------------------
 while not finish:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finish = True
 
     pressd = pygame.key.get_pressed()  # 키 이벤트
-    gametime = int(pygame.time.get_ticks())
+    gametime = int(pygame.time.get_ticks()) #게임 타이머
 
-    if Page == 1:
-        game_screen.fill((200, 200, 0))  # 배경색 갱신해줘야한다
+    if Page == 1:  #초기화면
+        game_screen.fill((200, 200, 0))  # 배경색
 
         # 텍스트 출력용
         show_text(game_screen, "Gamestart", 100, 100)
@@ -55,27 +65,24 @@ while not finish:
 
 
 
-    elif Page == 2:
-        #밑에 게임로직 설계
-        game_screen.fill((0, 200, 0))  #배경색 갱신해줘야한다
+    elif Page == 2: #게임 시작화면
+        game_screen.fill((0, 200, 0))  #배경색
 
         if pressd[pygame.K_RIGHT]:  x += 5
         if pressd[pygame.K_LEFT]:   x -= 5
-
-        show_img(game_screen, "picture/coin.png", x, y)
-
-        #임시로 준것
+        # 임시로 준것
         if pressd[pygame.K_DOWN]:   Page = 3
 
 
+        show_img(game_screen, "picture/coin.png", x, y)
+
+
         # 텍스트 출력용
-        show_text(game_screen, "Gametime : " + str(gametime), 100,100)
+        show_text(game_screen, "Gametime : " + str(gametime), 100, 100)
 
 
-    elif Page == 3:
-        game_screen.fill((200, 200, 200))  # 배경색 갱신해줘야한다
-
-
-
+    elif Page == 3: #엔딩 화면
+        game_screen.fill((200, 200, 200))  # 배경색
+        show_text(game_screen, "GameOver", 100, 100)
 
     pygame.display.flip() #프레임 갱신
