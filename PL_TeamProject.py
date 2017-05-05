b5f78d9b8d14e75b8deb7177c798b98d30eb408d
leechungrae <leechungrae@gmail.com>
2017-05-06 01:28:00 +0900

    arrange

--- PL_TeamProject.py
+++ PL_TeamProject.py
@@ -45,7 +45,6 @@ class Enemy:
         self.mVector = pygame.math.Vector2.normalize(self.mVector)
 
     def update(self):
-<<<<<<< HEAD
 
         global widthSize, heightSize , imagesize
         self.x += self.mVector[0]
@@ -57,26 +56,14 @@ class Enemy:
             self.mVector = pygame.math.Vector2(-self.vx, self.vy)
             self.mVector = pygame.math.Vector2.normalize(self.mVector)
 
-=======
-        global widthSize, heightSize
-        self.x += self.mVector[0]
-        self.y += self.mVector[1]
-        if self.x < 0 or self.y < 0:
-            self.check = True
-        if self.x > widthSize or self.y > heightSize:
-            self.check = True
->>>>>>> 8b11ec416427a11f7606b34c9364d8b5d19bee63
 
     def draw(self):  #나중에 부모클래스 하나 만들어서 상속받아도 될듯
         mRect = self.mImage.get_rect()
         mRect = mRect.fit((self.x, self.y, 50, 50))
         self.screen.blit(self.mImage, mRect)
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 8b11ec416427a11f7606b34c9364d8b5d19bee63
 #----------------------------------## 게임 기본 설정 관련 ##---------------------------------------
 
 
@@ -146,7 +133,6 @@ while not finish:
         game_screen.fill((0, 200, 0))  #배경색
 
         if pressd[pygame.K_RIGHT]:
-<<<<<<< HEAD
             if x < widthSize-imagesize:     x += 5
         elif pressd[pygame.K_LEFT]:
             if x > 0:                       x -= 5
@@ -154,19 +140,6 @@ while not finish:
             if y > 0:                       y -= 5
         elif pressd[pygame.K_DOWN]:
             if y < heightSize-imagesize:    y += 5
-=======
-            if x < widthSize-30:
-                x += 5
-        if pressd[pygame.K_LEFT]:
-            if x > 0:
-                x -= 5
-        if pressd[pygame.K_UP]:
-            if y > 0:
-                y -= 5
-        if pressd[pygame.K_DOWN]:
-            if y < heightSize-30:
-                y += 5
->>>>>>> 8b11ec416427a11f7606b34c9364d8b5d19bee63
 
         # 임시로 준것
         if pressd[pygame.K_p]:   Page = 3
@@ -187,15 +160,9 @@ while not finish:
 
         if makeEnemy == False:
             for i in range(10):
-<<<<<<< HEAD
                 random_x = random.randrange(0, widthSize-imagesize)
                 random_y = random.randrange(0, heightSize - imagesize)
                 enemy = Enemy(game_screen,random_x, random_y, x, y)  #200,200 은 방향을 지정하는거니깐 내쪽으로 움직이게 해야겠다
-=======
-                random_x = random.randrange(0, widthSize-30)
-                random_y = random.randrange(0, heightSize - 30)
-                enemy = Enemy(game_screen,random_x, random_y, 200, 200)
->>>>>>> 8b11ec416427a11f7606b34c9364d8b5d19bee63
                 enemyList.append(enemy)
 
             makeEnemy = True
