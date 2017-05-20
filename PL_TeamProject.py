4f02de2669b7bd78fe9ca8d9638e6f1addfc9238
leechungrae <leechungrae@gmail.com>
2017-05-21 01:41:40 +0900

    enemy speed

--- PL_TeamProject.py
+++ PL_TeamProject.py
@@ -20,8 +20,9 @@ class MoveEntity:
 
 
 class Enemy(MoveEntity):  # 상속
-    def __init__(self, screen, ex, ey, tx, ty):
+    def __init__(self, screen, ex, ey, tx, ty, gt):
         MoveEntity.__init__(self, screen, ex, ey, tx, ty)
+        self.gameTime = gt
         self.mImage = pygame.image.load("picture/enemy1.png")
         self.mImage = pygame.transform.scale(self.mImage, (30, 30))
 
@@ -30,8 +31,7 @@ class Enemy(MoveEntity):  # 상속
         MoveEntity.draw(self)
 
     def update(self):
-        self.x += self.mVector[0]
-        self.y += self.mVector[1]
+
 
         if self.y < 0 or self.y > heightSize - enemySize:
             self.vy = -self.vy
@@ -41,16 +41,13 @@ class Enemy(MoveEntity):  # 상속
         self.mVector = pygame.math.Vector2(self.vx, self.vy)
         self.mVector = pygame.math.Vector2.normalize(self.mVector)
 
-        #-------이부분 수정 필요하다 회전은 하지만 개체로 인식 못함------------
+        self.mVector[0] *= gameTime
+        self.mVector[1] *= gameTime
 
-        self.mImage2 = pygame.image.load("picture/enemy2.png")
-        self.mImage2 = pygame.transform.scale(self.mImage2, (30, 30))
-        r_x = random.randrange(0, widthSize)
-        r_y = random.randrange(0, heightSize)
-        angle = math.atan2(r_x - (self.y + 32), r_y - (self.x + 26))
-        playerrot = pygame.transform.rotate(self.mImage2, 360 - angle * 57.29)
-        playerpos1 = (self.y - playerrot.get_rect().width / 2, self.x - playerrot.get_rect().height / 2)
-        game_screen.blit(playerrot, playerpos1)
+        self.x += self.mVector[0]
+        self.y += self.mVector[1]
+
+
 
         # ------------------------------------------------------------
 
@@ -108,6 +105,7 @@ airplane_pos_x = widthSize / 2          # 내 캐릭터 초기 위치값
 airplane_pos_y = heightSize / 2         # 내 캐릭터 초기 위치값
 missileCheck = True
 
+gameResult = []
 
 #-----------------------------------## 게임 로직 시작 ##---------------------------------------
 finish = False
@@ -146,7 +144,7 @@ while not finish:
         if makeEnemy == False:
             for i in range(enemyCount):
                 (random_x, random_y) = function.enemyRandomPosition(widthSize, heightSize, enemySize)
-                enemy = Enemy(game_screen, random_x, random_y, airplane_pos_x, airplane_pos_y)
+                enemy = Enemy(game_screen, random_x, random_y, airplane_pos_x, airplane_pos_y, gameTime)
                 enemyList.append(enemy)
             makeEnemy = True
 
@@ -187,6 +185,7 @@ while not finish:
 
         if gameOverTimeCheck == False:
             gameOverTime = gameTime
+            gameResult.append(gameOverTime)
             gameOverTimeCheck = True
 
         function.show_text(game_screen, "GameOver:  " + str(gameOverTime), widthSize / 2, heightSize / 2)
@@ -195,12 +194,12 @@ while not finish:
             gameTimeCheck = False  # 종료시 게임시간 체크
             makeEnemy = False  # 처음에 2페이지 넘어가면 한번만 생성해주기위해서 판단하는 변수
             gameStartTimeCheck = False
+            gameOverTimeCheck = False
             enemyList = []  # 적군 리스트
             missileList = []  # 미사일 리스트
             page = 1  # 페이지 처리
             enemyCount = 7  # 적군 숫자
             airplane_pos_x = widthSize / 2  # 내 캐릭터 초기 위치값
             airplane_pos_y = heightSize / 2  # 내 캐릭터 초기 위치값
-
 
     pygame.display.flip()  # 프레임 갱신
